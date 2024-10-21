a6f00d8c558e8750a0e99fd56522bbc373884f3e
adityatripathi <aditya.tripathi@ivi.fraunhofer.de>
2024-10-21 09:58:33 +0200

    change code for python 3.10

--- fcord/coords.py
+++ fcord/coords.py
@@ -1,10 +1,20 @@
-from typing import Self
+from typing import TypeVar, List, Optional
 import math
 
+# Define a TypeVar that is bound to the Coordinate class.
+T = TypeVar('T', bound='Coordinate')
 
 class Coordinate:
     def __init__(self):
-        self.r = []
+        self._r: List[float] = []  # Use a private attribute for r
+
+    @property
+    def r(self) -> List[float]:
+        return self._r
+
+    @r.setter
+    def r(self, value: List[float]):
+        self._r = value
 
     def __len__(self) -> float:
         """Returns the l2 norm of the vector."""
@@ -13,48 +23,43 @@ class Coordinate:
     def l2_norm(self) -> float:
         return len(self)
 
-
 class CartesianCoord(Coordinate):
-    def __init__(self, x: float | int, y: float | int, z: float | int):
+    def __init__(self, x: float, y: float, z: float):
         super().__init__()
         self.x = float(x)
         self.y = float(y)
         self.z = float(z)
-
-    @property
-    def r(self) -> list[float]:
-        return [self.x, self.y, self.z]
+        self.r = [self.x, self.y, self.z]  # Set r during initialization
 
     def __repr__(self) -> str:
         return f"CartesianCoord({self.x}, {self.y}, {self.z})"
 
-    def __add__(self, other):
+    def __add__(self, other: 'CartesianCoord') -> 'CartesianCoord':
         if isinstance(other, CartesianCoord):
             return CartesianCoord(self.x + other.x, self.y + other.y, self.z + other.z)
         else:
             raise ValueError(f"Cannot add CartesianCoord to {type(other)}")
 
-    def __sub__(self, other):
+    def __sub__(self, other: 'CartesianCoord') -> 'CartesianCoord':
         if isinstance(other, CartesianCoord):
             return CartesianCoord(self.x - other.x, self.y - other.y, self.z - other.z)
         else:
             raise ValueError(f"Cannot subtract CartesianCoord from {type(other)}")
 
-
 class GPSCoord:
-    def __init__(self, lat: float | int, lon: float | int, alt: float | int):
+    def __init__(self, lat: float, lon: float, alt: float):
         self.lat = float(lat)
         self.lon = float(lon)
         self.alt = float(alt)
 
     @property
-    def r(self) -> list[float]:
+    def r(self) -> List[float]:
         return [self.lat, self.lon, self.alt]
 
     def __repr__(self) -> str:
         return f"GPSCoord({self.lat}, {self.lon}, {self.alt})"
 
-    def ecef(self):
+    def ecef(self) -> CartesianCoord:
         """Returns the ECEF coordinates of the GPS coordinates."""
         a = 6378137.0  # Semi-major axis
         f = 1 / 298.257223563  # Flattening
@@ -69,56 +74,48 @@ class GPSCoord:
         z = (N * (1 - e2) + self.alt) * math.sin(lat_rad)
         return CartesianCoord(x, y, z)
 
-    def distance(self, other: Self) -> float:
+    def distance(self, other: T) -> float:
         return (self.ecef() - other.ecef()).l2_norm()
 
-
 class ENUCoord(Coordinate):
-    def __init__(self, e: float | int, n: float | int, u: float | int, global_origin: GPSCoord = None):
+    def __init__(self, e: float, n: float, u: float, global_origin: Optional[GPSCoord] = None):
         super().__init__()
         self.global_origin = global_origin
         self.e = float(e)
         self.n = float(n)
         self.u = float(u)
-
-    @property
-    def r(self) -> list[float]:
-        return [self.e, self.n, self.u]
+        self.r = [self.e, self.n, self.u]  # Set r during initialization
 
     def __repr__(self) -> str:
         return f"ENUCoord({self.e}, {self.n}, {self.u})"
 
-    def to_ned(self) -> "NEDCoord":
+    def to_ned(self) -> 'NEDCoord':
         return NEDCoord(self.n, self.e, -self.u, self.global_origin)
 
-    def __add__(self, other) -> Self:
+    def __add__(self, other: 'ENUCoord') -> 'ENUCoord':
         if isinstance(other, ENUCoord):
-            return ENUCoord(self.e + other.e, self.n + other.n, self.u + other.u)
+            return ENUCoord(self.e + other.e, self.n + other.n, self.u + other.u, self.global_origin)
         elif isinstance(other, NEDCoord):
             return self + other.to_enu()
         else:
             raise ValueError(f"Cannot add ENUCoord to {type(other)}")
 
-    def __sub__(self, other) -> Self:
+    def __sub__(self, other: 'ENUCoord') -> 'ENUCoord':
         if isinstance(other, ENUCoord):
-            return ENUCoord(self.e - other.e, self.n - other.n, self.u - other.u)
+            return ENUCoord(self.e - other.e, self.n - other.n, self.u - other.u, self.global_origin)
         elif isinstance(other, NEDCoord):
             return self - other.to_enu()
         else:
             raise ValueError(f"Cannot subtract ENUCoord from {type(other)}")
 
-
 class NEDCoord(Coordinate):
-    def __init__(self, n: float | int, e: float | int, d: float | int, global_origin: GPSCoord = None):
+    def __init__(self, n: float, e: float, d: float, global_origin: Optional[GPSCoord] = None):
         super().__init__()
         self.global_origin = global_origin
         self.n = float(n)
         self.e = float(e)
         self.d = float(d)
-
-    @property
-    def r(self) -> list[float]:
-        return [self.n, self.e, self.d]
+        self.r = [self.n, self.e, self.d]  # Set r during initialization
 
     def __repr__(self) -> str:
         return f"NEDCoord({self.n}, {self.e}, {self.d})"
@@ -126,17 +123,17 @@ class NEDCoord(Coordinate):
     def to_enu(self) -> ENUCoord:
         return ENUCoord(self.e, self.n, -self.d, self.global_origin)
 
-    def __add__(self, other) -> Self:
+    def __add__(self, other: 'NEDCoord') -> 'NEDCoord':
         if isinstance(other, NEDCoord):
-            return NEDCoord(self.n + other.n, self.e + other.e, self.d + other.d)
+            return NEDCoord(self.n + other.n, self.e + other.e, self.d + other.d, self.global_origin)
         elif isinstance(other, ENUCoord):
             return self + other.to_ned()
         else:
             raise ValueError(f"Cannot add NEDCoord to {type(other)}")
 
-    def __sub__(self, other) -> Self:
+    def __sub__(self, other: 'NEDCoord') -> 'NEDCoord':
         if isinstance(other, NEDCoord):
-            return NEDCoord(self.n - other.n, self.e - other.e, self.d - other.d)
+            return NEDCoord(self.n - other.n, self.e - other.e, self.d - other.d, self.global_origin)
         elif isinstance(other, ENUCoord):
             return self - other.to_ned()
         else:
